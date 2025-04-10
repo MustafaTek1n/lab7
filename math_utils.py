a863a543a5404ff11e515d8949986d45115bd3d6
MUSTAFA TEKİN <152206620+MustafaTek1n@users.noreply.github.com>
2025-04-10 12:48:54 +0300

    Create math_utils.py

---
+++ math_utils.py
@@ -0,0 +1,24 @@
+def add(x,y):
+    return x+y
+    
+def subtract(x,y):
+    return x-y
+    
+def multiply(x,y):
+    return x*y
+    
+def divide(x,y):
+    if(y==0):
+        return "Eror"
+    else:
+        return x/y
+        
+def power(x,y):
+    return x**y
+    
+def mod(x,y):
+    if(y==0):
+        return "Eror"
+    else:
+        return x%y
+
